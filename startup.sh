6d4e7313b71a11e3a4bc0f297b94eb3edb16567f
hunterlong <info@socialeck.com>
2018-04-23 14:38:56 -0700

    info

--- startup.sh
+++ startup.sh
@@ -14,25 +14,25 @@ litecoin-cli -regtest $AUTH getblockchaininfo
 
 litecoin-cli -regtest $AUTH generate 110
 
-litecoin-cli -regtest importprivkey "cVVGgzVgcc5S3owCskoneK8R1BNGkBveiEcGDaxu8RRDvFcaQaSG" "Account" false
-litecoin-cli -regtest importprivkey "cRGkipHiYFRSAgdY9NjUT7egHTuNXoKYWQea3kWVbkSJAs4VDi8r" "Account" false
-litecoin-cli -regtest importprivkey "cTc8XCQZuSt5E1LArqCxyaXhn1cQkvcBMAGQ159raPSQTuBpHWdi" "Account" false
-litecoin-cli -regtest importprivkey "cQ9JwsoYHC2Md41uDbczDVpsuWAeYjDDrDiGbCBZ4stZhZvLZWj8" "Account" false
-litecoin-cli -regtest importprivkey "cQrY4VypAuemJtHmNNJLyx1SNjY7mpfkdQEJpccpLSvan5YoMAkM" "Account" true
+litecoin-cli -regtest importprivkey "cVVGgzVgcc5S3owCskoneK8R1BNGkBveiEcGDaxu8RRDvFcaQaSG" "Account1" false
+litecoin-cli -regtest importprivkey "cRGkipHiYFRSAgdY9NjUT7egHTuNXoKYWQea3kWVbkSJAs4VDi8r" "Account2" false
+litecoin-cli -regtest importprivkey "cTc8XCQZuSt5E1LArqCxyaXhn1cQkvcBMAGQ159raPSQTuBpHWdi" "Account3" false
+litecoin-cli -regtest importprivkey "cQ9JwsoYHC2Md41uDbczDVpsuWAeYjDDrDiGbCBZ4stZhZvLZWj8" "Account4" false
+litecoin-cli -regtest importprivkey "cQrY4VypAuemJtHmNNJLyx1SNjY7mpfkdQEJpccpLSvan5YoMAkM" "Account5" true
 
 litecoin-cli -regtest $AUTH generate 1
 
-litecoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 10 "" "" true
-litecoin-cli -regtest sendtoaddress "mzdF3oEx8mKrpGb5rVnTE7MhQfL8N8oSnW" 12 "" "" true
-litecoin-cli -regtest sendtoaddress "mtdVMhiWWmegkkBhzYDrz84yfgofPNLNmb" 14 "" "" true
-litecoin-cli -regtest sendtoaddress "mqNnZTyFxhB6EzF1iDEAp9enrT84fwd1X5" 17 "" "" true
-litecoin-cli -regtest sendtoaddress "mnk2URqujBqMEfhALMby1WZHoBRauW37Kg" 20 "" "" true
+litecoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 100
+litecoin-cli -regtest sendfrom "" "mzdF3oEx8mKrpGb5rVnTE7MhQfL8N8oSnW" 12
+litecoin-cli -regtest sendfrom "" "mtdVMhiWWmegkkBhzYDrz84yfgofPNLNmb" 14
+litecoin-cli -regtest sendfrom "" "mqNnZTyFxhB6EzF1iDEAp9enrT84fwd1X5" 17
+litecoin-cli -regtest sendfrom "" "mnk2URqujBqMEfhALMby1WZHoBRauW37Kg" 20
 
 litecoin-cli -regtest $AUTH generate 1
 
-litecoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 1 "" "" true
-litecoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 2 "" "" true
-litecoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 3 "" "" true
+litecoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 1
+litecoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 2
+litecoin-cli -regtest sendtoaddress "mnJQyeDFmGjNoxyxKQC6MMFdpx77rYV3Bo" 3
 
 litecoin-cli -regtest $AUTH generate 1
 
